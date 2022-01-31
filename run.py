56ef22e178c83330aee98db3e17f51e5e08a356f
robindavidgames <robin.david.games@gmail.com>
2022-01-31 23:44:50 +0000

    Outline required functions for programme.

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def see_target():
+    """
+    Present the daily writing target.
+    """
+
+def target_message():
+    """
+    Present a motivational message related to the user being on or off target.
+    """
+
+def total_words():
+    """
+    Present total number of words written.
+    """
+
+def log_day():
+    """
+    Update daily writing log.
+    """
+
+def prev_day():
+    """
+    Update a previous daily writing log.
+    """
+
+def see_progress():
+    """
+    See all progress made so far.
+    """
+
+def start():
+    """
+    On program launch.
+    """
