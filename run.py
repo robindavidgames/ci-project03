5e6522c1a7c41a1c68c3829120536f5c050bb725
robindavidgames <robin.david.games@gmail.com>
2022-01-31 23:49:28 +0000

    Add comments and whitespace.

--- run.py
+++ run.py
@@ -1,38 +1,53 @@
+"""
+Program to track a single user's writing progress for National Novel Writing
+Month.
+"""
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 
 def see_target():
     """
     Present the daily writing target.
     """
 
+
 def target_message():
     """
     Present a motivational message related to the user being on or off target.
     """
+
 
 def total_words():
     """
     Present total number of words written.
     """
 
+
 def log_day():
     """
     Update daily writing log.
     """
+
 
 def prev_day():
     """
     Update a previous daily writing log.
     """
 
+
 def see_progress():
     """
     See all progress made so far.
     """
 
+
 def start():
     """
     On program launch.
     """
+
+
+start()
