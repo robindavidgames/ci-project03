81c69b9046fe1776d948fc37ab6034f05e66bbbb
robindavidgames <robin.david.games@gmail.com>
2022-02-01 13:31:00 +0000

    Expand comments to clarify function purposes. Add instructional text.

--- run.py
+++ run.py
@@ -11,43 +11,62 @@ Month.
 def see_target():
     """
     Present the daily writing target.
+    Receives progress from target_message().
     """
 
 
 def target_message():
     """
     Present a motivational message related to the user being on or off target.
+    Recieves the total words and the number of completed dates.
+    Passes progress to see_target().
     """
 
 
 def total_words():
     """
     Present total number of words written.
+    Recieves current date.
+    Passes total words and current date to target_message().
     """
 
 
 def log_day():
     """
     Update daily writing log.
+    Add a new line to the worksheet to represent the current day.
+    Passes current date to total_words().
     """
 
 
 def prev_day():
     """
     Update a previous daily writing log.
+    Allow user to input a previous date and then change the value in that date.
+    Passes current date to total_words()
     """
 
 
 def see_progress():
     """
     See all progress made so far.
+    Print the values for all dates input so far.
+    Passes current date to total_words()
     """
 
 
-def start():
+def main():
     """
     On program launch.
+    Provide 4 options to user:
+    log update; change previous log; see daily goal; see all progress.
     """
+    print("Choose which action you would like to perform. (Type number 1, 2, 3, or 4)")
+    print("1. Add a new day's progress.")
+    print("2. Update a previous day's progress.")
+    print("3. See your daily goal.")
+    print("4. See all your progress.\n")
 
 
-start()
+print("Welcome to your National Novel Writing Month progress tracker.\n")
+main()
